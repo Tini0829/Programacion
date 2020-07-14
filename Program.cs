1d49753534592cf6f0f642b14ae5fd92b7101ae6
Tini0829 <68297967+Tini0829@users.noreply.github.com>
2020-07-14 17:24:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EJERCICIO_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//ejercicio 02-01 conociendo y, z
+			double y = 2;
+			double z = 3;
+
+			//para encontrar "a" 
+			double aRad = Math.Atan(y / z);
+			double aDregress = aRad * 180 / Math.PI;
+			Console.WriteLine("a:" + aDregress);
+
+			//para calcular "c"
+			double cRad = Math.Atan(z / y);
+			double cDregress = cRad * 180 / Math.PI;
+			Console.WriteLine("c:" + cDregress);
+
+			//para encontrar "t"
+			double H = Math.Sqrt(Math.Pow(y, 2) + Math.Pow(z, 2));
+			Console.WriteLine("t:" + H);
+		}
+
+	}
+}
