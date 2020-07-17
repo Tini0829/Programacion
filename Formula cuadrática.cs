2ac6aaf3373c51b431a4c86d67b925be27473992
Tini0829 <68297967+Tini0829@users.noreply.github.com>
2020-07-17 16:50:50 -0500

    Add files via upload

---
+++ Formula cuadrática.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Formula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa B: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa C: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double discriminante = (Math.Pow(b, 2) - (4 * a * c));
+
+            if (discriminante == 0)
+            {
+                double positivo = (-b) / (2 * a);
+                Console.WriteLine("LA SOLUCIÓN ES ÚNICA");
+                Console.WriteLine("SOLUCIÓN:" + positivo);
+            }
+            else if (discriminante > 0)
+            {
+                double positivo = ((-b) + Math.Sqrt(discriminante) )/ (2*a);
+                double negativo = ((-b) - Math.Sqrt(discriminante)) / (2 * a);
+                Console.WriteLine("EXISTEN DOS SOLUCIONES");
+                Console.WriteLine("SOLUCIÓN 1:" + positivo);
+                Console.WriteLine("SOLUCIÓN 2:" + negativo);
+            }
+            else
+            {
+                Console.WriteLine("NO HAY SOLUCIÓN");
+            }
+        }
+    }
+}
---
+++ Indice de Masa Corporal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu peso en Kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa tu altura en centimetros: ");
+            double estatura = double.Parse(Console.ReadLine());
+            double estaturam = estatura * (1.0 / 100);
+
+            double imc = peso / Math.Pow(estaturam, 2);
+            double imc1 = Math.Ceiling(imc);
+            Console.WriteLine("TU INDICE DE MASA CORPORAL ES:  " + imc1);
+
+            if (imc <= 18.5)
+            {
+                Console.WriteLine("¡Estas por debajo del peso que deberías!");
+            }
+            else if (18.5 < imc && imc <= 24.9)
+            {
+                Console.WriteLine("¡Tu peso es normal, felicitaciones!");
+            }
+            else if (24.9 < imc && imc <= 29.9 )
+            {
+                Console.WriteLine("¡Estas por encima del peso que deberías!");
+            }
+            else
+            {
+                Console.WriteLine("¡Tienes sobrepeso, deberias ir al doctor!");
+            }
+        }
+    }
+}
