b74998a25d2ffedb2e44ab1f6673e9f911f9022f
Tini0829 <68297967+Tini0829@users.noreply.github.com>
2020-07-30 19:15:11 -0500

    Add files via upload

---
+++ BlackJack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random aleatorio = new Random();
+            int carta = 0, total = 0, partida = 0;
+            string respuesta = "si";
+
+            while (respuesta == "si" && total < 22)
+            {
+                if (partida == 0)
+                {
+                    carta = aleatorio.Next(1, 10);
+                    Console.WriteLine(carta);
+                    total = (carta + total);
+                    carta = aleatorio.Next(1, 10);
+                    Console.WriteLine(carta);
+                    total = (carta + total);
+                    Console.WriteLine("Total:" + total);
+
+                    Console.WriteLine("Si quiere una carta presionan Si, si no presiona No:");
+                    respuesta = Console.ReadLine();
+                }
+                else
+                {
+                    carta = aleatorio.Next(1, 10);
+                    Console.WriteLine(carta);
+                    total = (carta + total);
+                    Console.WriteLine("Total:" + total);
+
+                    if (total > 21)
+                    {
+                        Console.WriteLine("No puede continuar");
+                    }
+
+                    else if (total == 21)
+                    {
+                        Console.WriteLine("¡Ganaste!Eres un pro:");
+                        respuesta = "No";
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Si quiere una carta presionan Si, si no presiona No:");
+                        respuesta = Console.ReadLine();
+
+                    }
+                }
+                partida = (partida + 1);
+            }
+        }
+    }
+}
