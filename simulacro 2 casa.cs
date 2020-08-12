5e7ab7d02e80caa7fee56b753901da9d2d9baf90
Tini0829 <68297967+Tini0829@users.noreply.github.com>
2020-08-12 17:44:25 -0500

    Add files via upload

---
+++ simulacro 2 casa.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace simulacro_2___casa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*Entradas
+            Random aleatorio = new Random();
+            int d = 0, d2 = 0, total = 0, vida = 3, turnosvida = 0, turnosdados = 0;
+            string continuar = "si";
+            Console.WriteLine("Bienvenido al juego");
+
+            while (continuar == "si" && total < 100)
+            {
+                //Turnos 
+                turnosdados += 1;
+                turnosvida += 1;
+
+                //Dos dados, turno 3 y suma de vidas
+                if (turnosdados == 3)
+                {
+                    turnosdados = 0;
+                    d = 3;
+                    d2 = 3;
+                    Console.WriteLine("Dado 1: " + d);
+                    Console.WriteLine("Dado 2: " + d2);
+                    total += d + d2;
+
+                    if (d == d2)
+                    {
+                        vida += 1;
+                        Console.WriteLine("+1 de vida");
+                        Console.WriteLine("Tienes " + vida + " vida");
+                    }
+                }
+                else
+                {
+                    d= aleatorio.Next(1, 7);
+                    Console.WriteLine("Dado: " +d);
+                    total += d;
+                }
+                //perder vida
+                if (turnosvida==2)
+                {
+                    turnosvida = 0;
+                    vida -= 1;
+                    Console.WriteLine("-1 de vida");
+                    Console.WriteLine("Te quedan "+vida+" vida");
+
+                    if (vida == 0)
+                    {
+                        Console.WriteLine("Fuiste eliminado, eres un perdedor :(");
+                        break;
+                    }
+                }
+                //Total
+                Console.WriteLine("Total: "+total);
+
+                //Ganar
+                if(total > 99)
+                {
+                    Console.WriteLine("Eres lo máximo, ganaste");
+                    break;
+                }
+
+                //Continuación del juego
+                else
+                {
+                    Console.WriteLine("¿Deseas continuar? (si/no)");
+                    continuar= Console.ReadLine();
+                    if (continuar == "no") break;
+                }
+            }
+
+            Console.WriteLine("Tu total fue: " + total);
+            Console.WriteLine("Gracias por participar");
+        }
+    }
+}
---
+++ simulacro 2 clase.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace simulacro_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*Entradas
+            Random aleatorio = new Random();
+            int d = 0, total = 0, uno = 0, seis = 0, vida = 3, especial = 2, despecial = 0;
+            string continuar = "si", UsarEspecial = "no";
+            Console.WriteLine("Bienvenido al juego");
+
+            while (continuar == "si" && vida > 0 && total < 100)
+            {
+                if (especial > 0)
+                {
+                    Console.WriteLine("Tienes " + especial + " dados especiales");
+                    Console.WriteLine("¿Deseas usar uno? (si/no): ");
+                    UsarEspecial = Console.ReadLine();
+
+                    if (UsarEspecial == "si")
+                    {
+                        despecial = aleatorio.Next(1, 13);
+                        total += despecial;
+
+
+                        if (continuar == "no")
+                            break;
+                         especial--;
+
+                        if (despecial == 1) uno++;
+                        if (despecial == 6) seis++;
+                        else seis = 0;
+                        if (vida <= 3 && seis == 2) vida++;
+
+                        if (uno == 2)
+                        {
+                            //operación de unos;
+                            vida--;
+                            if (total >= 10) total -= 10;
+                            else total = 0;
+
+                        }
+
+                        if (especial == 0) Console.WriteLine("Ya no tienes más dados especiales");
+                        Console.WriteLine("Tu dado especial: " + despecial);
+                        Console.WriteLine("Tu total es de: " + total);
+                        Console.WriteLine("¿Deseas continuar? (si/no): " + continuar);
+
+                        continuar = Console.ReadLine();
+                    }
+                }
+
+                if (UsarEspecial == "no" || especial == 0)
+                {
+                    d = aleatorio.Next(1, 7); //6
+                    total += d;
+
+
+                    if (d == 1) uno++;
+                    if (d == 6 || seis != 2) seis++;
+                    else seis = 0;
+                 
+
+                    //vidas
+                    if (vida <= 3 && seis % 2 == 0) vida++;
+                    if (uno == 2)
+                    {
+                        //operación de unos;
+                        vida--;
+                        if (total >= 10) total -= 10;
+                        else total = 0;
+
+                    }
+                    if (vida == 0)
+                    {    //Perdedor
+                        Console.WriteLine("Tu dado es: " + d);
+                        Console.WriteLine("Tu total es de: " + total);
+                        Console.WriteLine("¡Eres lo un loser, perdedor!");
+                        break;
+                    }
+
+                    //Salidas
+                    Console.WriteLine("Tu dado es: " + d);
+                    Console.WriteLine("Tu total es de: " + total);
+                    Console.WriteLine("Tu vida es de: " + vida);
+                    Console.WriteLine("¿Deseas continuar? (si/no): " );
+                    continuar = Console.ReadLine();
+                }
+            }
+            //Ganador
+            if (total > 100) 
+           Console.WriteLine("¡Eres lo máximo, ganaste!");
+           Console.WriteLine("Tu total es de: " + total);
+        }
+    }
+}
+
