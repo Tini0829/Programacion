69bb447a209f01285482bd1c3f70a13d79507f10
Tini0829 <68297967+Tini0829@users.noreply.github.com>
2020-08-19 13:30:16 -0500

    Add files via upload

---
+++ Reto T11-R02 SIN(X).cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Reto_T11_R02_SIN_X_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de x");
+            double x = double.Parse(Console.ReadLine());
+            double sin = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                double numerador = Math.Pow(-1, i);
+                double denominador = Factorial (2 * i + 1);
+                sin += (numerador / denominador) * Math.Pow (x, 2 * i + 1);
+
+            }
+            //*double sen1 = Math.Floor(sen); double sin1 = Math.Ceiling(sin);
+
+            Console.WriteLine("La respuesta es: "+sin);
+
+        }
+       static double Factorial(double valor)
+        {
+            if (valor == 1) return 1;
+            return valor * Factorial(valor - 1);
+        }
+    }
+}
